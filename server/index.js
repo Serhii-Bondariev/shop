f2ab123264011259c8c2295fd6b4e5ac7d49830e
Serhii <xx.code4u@gmail.com>
2024-01-09 18:47:05 +0200

    Routes done!

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require('./models/models')
+const cors = require('cors');
+const router = require('./routes/index')
 
-const  PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
-app.listen(PORT, () => console.log(`Сервак їбаше на ${PORT} порту!`))
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Сервак їбаше на ${PORT} порту!`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start()
+
